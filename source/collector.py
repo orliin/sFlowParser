eb0bc226b9c6bd2e4eadf5857fd74d8a5d9f7fd3
orliin <orliin@gmail.com>
2016-04-16 12:13:04 +0300

    Added RealTimeCollector concept

--- source/collector.py
+++ source/collector.py
@@ -5,6 +5,7 @@ from socket import socket, AF_INET, SOCK_DGRAM
 from xdrlib import Unpacker
 
 from source.sflowparse.SFlowDatagram import SFlowDatagram
+from source.collector.RealTimeCollector import RealTimeCollector
 
 MODE = '-file'  # -socket, -socket+file or -file
 RAW_DATA_FILENAME = 'raw.dat'
@@ -55,18 +56,21 @@ def get_raw_data(mode, bytes_deque):
 if __name__ == '__main__':
     logging.basicConfig(filename='debug.log', filemode='w', level=logging.DEBUG)
 
-    bytes_deque_global = deque()
+    sflow_datagram = None
+    real_time_collector = RealTimeCollector()
 
+    bytes_deque_global = deque()
     for raw_data in get_raw_data(MODE, bytes_deque_global):
 
         unpacker = Unpacker(raw_data)
 
         try:
             sflow_datagram = SFlowDatagram(unpacker)
-            # print(sflow_datagram['agent_ip_address'])
-            #if sflow_datagram['agent_ip_address'] == '10.10.0.21':
-            print(sflow_datagram)
-                # print("###########################################")
         except Exception as e:
             logging.warning("Bad sflow datagram: {}".format(raw_data))
+            continue
             raise e
+
+        print(sflow_datagram)
+
+        real_time_collector.add(sflow_datagram.data)
